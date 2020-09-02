48ae14166429dde5b5da1cf7eccf535cc2f43a39
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-02 17:10:06 +0200

    dot-problem
    js-jq-carousel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-
+//1. make a function which can change the img inside of the images class, by clicking on the next arrow
      $(".next").click(
-       function() {
+       function nextImg() {
+        var imgActive = $("img.active");
+        imgActive.removeClass("active");
+
+        if (imgActive.hasClass("last")) {
+          var imgNext = $("img.first");
+        } else {
+          var imgNext = imgActive.next();
+        }
+        imgNext.addClass("active");
+      }
+     );
+//2. make a function which can change the img inside of the images class, by clicking on the prev arrow
+     $(".prev").click(
+       function prevImg() {
         var imgActive = $("img.active");
         imgActive.removeClass("active");
 
@@ -14,17 +28,9 @@ $(document).ready(function () {
       }
      );
 
-     $(".prev").click(
-       function() {
-        var imgActive = $("img.active");
-        imgActive.removeClass("active");
-
-        if (imgActive.hasClass("last")) {
-          var imgNext = $("img.first");
-        } else {
-          var imgNext = imgActive.next();
-        }
-        imgNext.addClass("active");
-      }
+     $(".nav i.fas.fa-circle active").click(
+       function dotSelection() {
+         $(this).next();
+       }
      );
 });
