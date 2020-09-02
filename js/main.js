b589c8698f01df8511a196e1822e339123765243
Domenico Capalbo <capalbodomenico@gmail.com>
2020-09-02 21:38:25 +0200

    exercise-complete
    js-jq-carousel

--- js/main.js
+++ js/main.js
@@ -1,44 +1,58 @@
 $(document).ready(function () {
-//1. make a function which can change the img inside of the images class, by clicking on the next arrow
-     $(".next").click(
-       function nextImg() {
-        var activeCircle = $(".nav i.active");
-        activeCircle.removeClass("active");
+//1. make a function with a click alert, to invoke the nextImg function
 
-        var imgActive = $("img.active");
-        imgActive.removeClass("active");
-
-    if (imgActive.hasClass("last")) {
-        (activeCirlce.hasClass("last"))
-          var imgNext = $("img.first");
-          var circleNext = $(".nav i.first");
-        } else {
-          var imgNext = imgActive.next();
-          var circleNext = activeCircle.next();
-          imgNext.addClass("active");
-          circleNext.addClass("active");
-        }
+     $(".next").click(function () {
+        nextImg();
       }
      );
-//2. make a function which can change the img inside of the images class, by clicking on the prev arrow
-     $(".prev").click(
-       function prevImg() {
-        var activeCircle = $(".nav i.active");
-        activeCircle.removeClass("active");
+//2. make a function with a click alert, to invoke the prevImg function
 
-        var imgActive = $("img.active");
-        imgActive.removeClass("active");
-
-    if (imgActive.hasClass("last")) {
-       (activeCirlce.hasClass("last"))
-         var imgNext = $("img.first");
-         var circleNext = $(".nav i.first");
-        } else {
-         var imgNext = imgActive.next();
-         var circleNext = activeCircle.next();
-         imgNext.addClass("active");
-         circleNext.addClass("active");
-       }
+     $(".prev").click( function () {
+        prevImg();
       }
-    );
+     );
+//3. make a function which can change the img inside of the images class, by clicking on the next arrow
+
+     function nextImg() {
+       var activeCircle = $(".nav .active");
+       activeCircle.removeClass("active");
+       var imgActive = $("img.active");
+       imgActive.removeClass("active");
+
+       if (imgActive.hasClass("last") && activeCircle.hasClass("last")) {
+          $("img.first").addClass("active");
+          $(".nav .first").addClass("active");
+       } else {
+         imgActive.next().addClass("active");
+         activeCircle.next().addClass("active");
+       }
+     }
+//4. make a function which can change the img inside of the images class, by clicking on the prev arrow
+
+     function prevImg() {
+       var activeCircle = $(".nav .active");
+       activeCircle.removeClass("active");
+       var imgActive = $("img.active");
+       imgActive.removeClass("active");
+
+       if (imgActive.hasClass("first") && activeCircle.hasClass("first")) {
+          $("img.last").addClass("active");
+          $(".nav .last").addClass("active");
+       } else {
+         imgActive.prev().addClass("active");
+         activeCircle.prev().addClass("active");
+       }
+     }
+
+//5. make a function to define how to scroll the images with the keyboard buttons
+
+     $(document).keydown(
+          function(e){
+            if(e.keyCode == 37){
+              prevImg();
+            } else if (e.keyCode == 39){
+              nextImg();
+            }
+          }
+        );
 });
